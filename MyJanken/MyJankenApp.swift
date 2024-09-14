0ed24bd38ff14607983cb4b50b4edad9a1f29c0a
JunichiroM <sbxn4mxt@yahoo.co.jp>
2024-09-14 13:21:29 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by apple on 2024/09/14.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
